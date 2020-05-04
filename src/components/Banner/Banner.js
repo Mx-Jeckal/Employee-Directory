fbda723c1a6ed2afcbbe1f6aba30a0ed8baf5977
Hazel Tuxhorn <mx.jeckal@gmail.com>
2020-05-03 20:30:57 -0500

    added banner component

---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css"
+
+
+
+const Banner = (props) => {
+  return ( 
+    <div id="bannerimage"></div>
+  )
+}
+
+export default Banner;
--- src/components/Table/MyTable.js
+++ src/components/Table/MyTable.js
@@ -3,6 +3,7 @@ import Thead from './Thead.js';
 import emp from '../emp-dir/generated.json';
 import {Table, Container} from "react-bootstrap";
 import Tbody from './Tbody.js';
+import Banner from './Banner.js'
 
 class MyTable extends Component {
 
@@ -17,17 +18,23 @@ class MyTable extends Component {
     this.setState({ ...this.state, search: event.target.value ,displayEmp: this.state.emp.filter(entry => entry.name.toLowerCase().indexOf(this.state.search) !== -1) })
   }
 
+    reset = () => {
+      this.setState({ ...this.state, search: '', displayEmp: emp})
+    }
     render() {
         return (
             <Container>
+            <Banner />
             <div>
             <input type='text' value={this.state.search} onChange={this.filter}></input>
+            <button onClick={this.reset}>Reset</button>
             </div>
             
            <Table responsive hover>
             <Thead />
             {this.state.displayEmp.map(emp => (
             <Tbody
+              key={emp._id}
               image={emp.picture}
               name={emp.name}
               phone={emp.phone}
